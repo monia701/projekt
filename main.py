dec4462b78828846fcbc5d3199d646f17ff0bb0c
Monika Grzesło <monika.grzeslo@example.com>
2025-06-23 19:00:22 +0200

    Dodano kamerę i poruszanie tłem

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import pygame
+import random
+from settings import *
+from player import Player
+
 # --- Inicjalizacja pygame ---
 pygame.init()
 pygame.mixer.init()
-
-# --- EKRAN ---
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Gra kosmos")
-clock = pygame.time.Clock()
 
 # --- MUZYKA ---
 pygame.mixer.music.load("assets/sounds/MyVeryOwnDeadShip.ogg")
 pygame.mixer.music.play(-1)  # -1 = zapętlanie
 
+# --- EKRAN ---
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Gra kosmos")
+clock = pygame.time.Clock()
 
 # --- TŁO ---
 bg = pygame.image.load("assets/images/arena.png").convert()
@@ -19,6 +23,7 @@ scroll_x = 0
 
 # --- CZCIONKA ---
 font = pygame.font.SysFont("arial", 36)
+
 
 # --- Funkcje menu ---
 def show_start_menu():
@@ -35,6 +40,7 @@ def show_start_menu():
     screen.blit(start_msg, ((WIDTH - start_msg.get_width()) // 2, HEIGHT // 2 + 100))
 
     pygame.display.flip()
+
 
 def show_end_menu(winner):
     screen.fill((0, 0, 0))
@@ -63,26 +69,45 @@ def show_end_menu(winner):
                     exit()
     return play_again
 
+
 # --- Funkcja zapisu wyniku ---
 def save_result(winner):
     with open("wyniki.txt", "a") as file:
         file.write(f"{winner} wygrał\n")
 
-       # --- Kamera ---
-        center_between_players = (player1.rect.centerx + player2.rect.centerx) // 2
-        scroll_x = center_between_players - WIDTH // 2
-        scroll_x = max(0, min(scroll_x, bg_width - WIDTH))
 
-        screen.blit(bg, (-scroll_x, 0))
+game_running = True
+while game_running:
+    show_start_menu()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                game_running = False
+    # --- RESET graczy ---
+    player1 = Player(400, HEIGHT - PLAYER_HEIGHT - 50, "Gracz1", pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_f)
+    player2 = Player(1200, HEIGHT - PLAYER_HEIGHT - 50, "Gracz2", pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP,
+                     pygame.K_RETURN)
 
-        keys = pygame.key.get_pressed()
-        player1.handle_input(keys)
-        player2.handle_input(keys)
+    # --- Power-up ---
+    heart_timer = 300  # co ile klatek pojawia się serduszko (~5 sekund)
+    heart = None
 
-        player1.update()
-        player2.update()
+    # --- Kamera ---
+center_between_players = (player1.rect.centerx + player2.rect.centerx) // 2
+scroll_x = center_between_players - WIDTH // 2
+scroll_x = max(0, min(scroll_x, bg_width - WIDTH))
+
+screen.blit(bg, (-scroll_x, 0))
+
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        running = False
+        game_running = False
+
+keys = pygame.key.get_pressed()
+player1.handle_input(keys)
+player2.handle_input(keys)
+
+player1.update()
+player2.update()
+
+
+# --- Koniec gry ---
+pygame.quit()
